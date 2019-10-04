e648a31e1ccc56dbb85d72fbfd0f7f605a5f2ca4
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-10-04 10:51:26 +0300

    added feature pregerences

--- static/js/preferences.js
+++ static/js/preferences.js
@@ -8,33 +8,57 @@ let checkBoxes = [
         '#defaultCheck7',
 ]
 
-
-function getStorageItems(){
-    let iter = ''
-    let lsiter = ''
+// функция берет из LocalStorage данные и заполняет чекбоксы
+// To Do.
+// - переименование функции в более понятное
+// - переименование переменных
+// - передавать в функцию массив имен как параметр
+function getStorageItems() {
+    let iter = ''           //итератор по id чекбоксов
+    let LSiter = ''         //итератор по сохраненным  в localStorage предметам
     for (i in checkBoxes) {
         iter = $(checkBoxes[i])
-        lsiter = localStorage.getItem(checkBoxes[i])
-        iter.prop('checked', lsiter)
+        LSiter = localStorage.getItem(checkBoxes[i])
+        if (LSiter === 'true') {
+            iter.prop('checked', true)
+        } else {
+            iter.prop('checked', false)
+        }
     }
 }
 
+// функция состояние чекбоксов и сохраняет его в LocalStorage
+// To Do.
+// - переименование функции в более понятное
+// - переименование переменных
+// - передавать в функцию массив имен как параметр
 function setStorageItems(){
     let iter = ''
-    let lsiter = ''
     for (i in checkBoxes) {
         iter = $(checkBoxes[i]).prop('checked')
         localStorage.setItem(checkBoxes[i], iter)
     }
 }
 
-let secondCheck = $('#defaultCheck6')
-secondCheck.prop("checked")
-secondCheck.prop("checked", true)
-
+// деактивируем чекбоксы из массива
+// To Do.
+// - передавать в функцию массив имен как парамет
+function disableCheckboxes() {
+    for (i in checkBoxes) {
+        $(checkBoxes[i]).prop('disabled', true)
+    }
+}
 
 function init() {
-    // $('#preferencesID').addClass('active');
+    getStorageItems()
+    $('#defaultCheck7').click(function() {
+        setStorageItems()
+        disableCheckboxes()
+    })
+    if ($('#defaultCheck7').prop('checked')) {
+        disableCheckboxes()
+    }
+
 }
 
 $(document).ready(init);
